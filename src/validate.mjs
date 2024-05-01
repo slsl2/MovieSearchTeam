569a99adc0e161dee15ae51d977b85e42ae442c2
computer-note <planomenes@gmail.com>
2024-05-01 16:46:36 +0900

    [validate-feature] validateSearch, validateReview 함수 구현

--- src/validate.mjs
+++ src/validate.mjs
@@ -1,25 +1,33 @@
-//validator 형식
-//[테스트 함수, 검사 결과 코드이름, 검사 결과 디폴트 메시지]
-const validator1 = [
-	() => {
-	
-	
-	 },
-	'bad-word',
-	'비속어를 사용하지 말아주세요.'
-];
-
-//validators 역할
-//validator의 코드이름을 key로 해서 각 validator를 저장
 const validators = {
-	[validator1[1]]: validator1,
+	//[테스트 함수, 검사 결과 코드, 검사 결과 디폴트 메시지]
+	'bad-word': [
+		(text) => text.includes('나쁜말'),
+		'bad-word',
+		'비속어를 사용하지 말아주세요.'
+	],
+	'hacker': [
+		(text) => /[<>]/.test(text),
+		'hacker',
+		'<>문자는 입력할 수 없습니다'
+	],
+	'not-fit': [
+		(text) => text.length < 1 || text.length > 10,
+		'not-fit',
+		'1자 이상 10자 이하로 입력해주세요.'
+	],
+	'not-fit-longer': [
+		(text) => text.length < 2 || text.length > 20,
+		'not-fit-longer',
+		'2자 이상 20자 이하로 입력해주세요.'
+	],
 };
 
 //반환 형식: [result code, result default message]
 //예시: ['not-fit', '1자 이상 10자 이하로 입력해주세요.']
 export function validateSearch(text) {
 	const myValidators = [
-
+		validators['hacker'],
+		validators['not-fit'],
 	];
 
 	return executeValidators(myValidators, text);
@@ -27,7 +35,9 @@ export function validateSearch(text) {
 
 export function validateReview(text) {
 	const myValidators = [
-
+		validators['hacker'],
+		validators['not-fit-longer'],
+		validators['bad-word'],
 	];
 
 	return executeValidators(myValidators, text);
@@ -40,4 +50,14 @@ function executeValidators(validators, testValue) {
 		}
 	}
 	return ['ok'];
+}
+
+//demonstrate();
+function demonstrate() {
+	console.log(validateReview('1'));
+	console.log(validateReview('<script>'));
+	console.log(validateReview('나쁜말'));
+
+	console.log(validateSearch(''));
+	console.log(validateSearch('<hackcode>'));
 }
