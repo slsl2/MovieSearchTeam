00fdab180db6ae3812647415a2f4d87cef656d5b
slsl2 <kimminji7153@gmail.com>
2024-05-07 12:28:51 +0900

    update: 검색창 유효성 검사 적용

--- src/search.js
+++ src/search.js
@@ -1,9 +1,12 @@
+import { validateSearch } from "./validate.mjs";
+
 export const searchMovie = () => {
   const cardList = document.querySelector("#mycards");
   document.addEventListener("DOMContentLoaded", function () {
     const searchBox = document.querySelector("#searchInput");
     searchBox.addEventListener("keyup", function () {
       // keyup : 키를 놓을 때 이벤트 발생
+
       const filterValue = searchBox.value.toLowerCase();
       const cards = cardList.querySelectorAll(".col");
       cards.forEach(function (card) {
@@ -16,6 +19,15 @@ export const searchMovie = () => {
           card.style.display = "none";
         }
       });
+      logger(validateSearch, filterValue);
     });
   });
+
+  function logger(validateFn, param) {
+    const [code, msg] = validateFn(param);
+    const searchValidation = document.querySelector("#search-validation");
+    msg
+      ? (searchValidation.textContent = `['${msg}']`)
+      : (searchValidation.textContent = "");
+  }
 };
